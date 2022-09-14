c55cb669edb8557ee2ad3e885f4b8b514acadac9
Jovian <jovian.marvin@ui.ac.id>
2022-09-14 14:41:24 +0700

    ya

--- katalog/models.py
+++ katalog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 class CatalogItem(models.Model):
     item_name = models.CharField(max_length=255)
     item_price = models.BigIntegerField()
     item_stock = models.IntegerField()
     description = models.TextField()
     rating = models.IntegerField()
+    img_url = models.URLField()
     item_url = models.URLField()
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+from django.shortcuts import render
+
+
+# Create your views here.
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_katalog_item,
+        'name': 'Jovian',
+        'student_id': '2106720891'
+    }
+
+    return render(request, "katalog.html", context)
+
 
 # TODO: Create your views here.
