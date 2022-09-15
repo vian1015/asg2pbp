b82afa86d5cdf3ad1ecaf94422655bd568796f1b
Jovian <jovian.marvin@ui.ac.id>
2022-09-15 11:04:39 +0700

    uwu

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
 
-# Create your tests here.
+
+class ContohAppTest(TestCase):
+    def test_contoh_app_url_exists(self):   
+        response = Client().get('/katalog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contoh_app_using_to_do_list_template(self):
+        response = Client().get('/katalog/')
+        self.assertTemplateUsed(response, 'katalog.html')
