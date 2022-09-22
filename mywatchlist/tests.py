cca921284e0f67ff6eeef902f97747c58278aa5f
Jovian <jovian.marvin@ui.ac.id>
2022-09-22 18:02:28 +0700

    asg 3 update

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -3,10 +3,26 @@ from django.urls import resolve
 
 
 class ContohAppTest(TestCase):
-    def test_contoh_app_url_exists(self):   
-        response = Client().get('/mywatchlist/')
+    def html_url_exists(self):   
+        response = Client().get('/mywatchlist/html/')
         self.assertEqual(response.status_code, 200)
 
-    def test_contoh_app_using_to_do_list_template(self):
-        response = Client().get('/mywatchlist/')
+    def html_using_to_do_list_template(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertTemplateUsed(response, 'mywatchlist.html')
+        
+    def xml_url_exists(self):   
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+
+    def xml_using_to_do_list_template(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertTemplateUsed(response, 'mywatchlist.html')
+
+    def json_url_exists(self):   
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+
+    def json_using_to_do_list_template(self):
+        response = Client().get('/mywatchlist/json/')
         self.assertTemplateUsed(response, 'mywatchlist.html')
