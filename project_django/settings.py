594ff795094b7ea59eaafcf51316740538bbfb2f
vian1015 <jovian.marvin@ui.ac.id>
2022-11-23 18:15:02 +0700

    cors

--- project_django/settings.py
+++ project_django/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
